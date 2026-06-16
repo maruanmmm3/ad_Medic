5545f015dbf3d3040662523aca01eb9b661db739
Jheanpier Maruan Aguilar Avila <jheanpier.avila@ex.gerdau.com>
2026-06-15 20:03:17 -0500

    Actualizacion 15-06

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <Link to="/">Inicio</Link>
+
+      <Link to="/productos">Productos</Link>
+
+      <Link to="/contacto">Contacto</Link>
+    </>
+  );
+}
